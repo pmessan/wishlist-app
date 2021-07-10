282ed9f6f3a871f6c121ec37fc18416bf94252f9
pmessan <p.newman.messan@gmail.com>
2021-07-10 18:12:48 +0000

    Item component image fix

--- components/Item.jsx
+++ components/Item.jsx
@@ -4,10 +4,11 @@ function Item({ wishlistItem }) {
     return (
         <div className=" mt-5 rounded-xl mx-6 h-24 bg-gray-300 grid grid-cols-4">
             <div className="relative col-span-1 w-full object-cover object-center">
-                <Image src={wishlistItem.imageUrl} alt={wishlistItem.imageAlt} layout="fill" objectFit="scale-down" />
+                <Image className="rounded-l-xl" src={wishlistItem.imageUrl} alt={wishlistItem.imageAlt} layout="fill" objectFit="fill" />
             </div>
-            <div className="block col-span-3 py-3 px-4">
-                <p className="sm:text-md xl:text-xl text-center">{wishlistItem.description}</p>
+            <div className="block col-span-3 py-1 px-4">
+                <p className="sm:text-md xl:text-xl text-left">{wishlistItem.description}</p>
+                <p className="sm:text-xs xl:text-md text-left">{wishlistItem.buylink}</p>
             </div>
         </div>
     );
--- data/items.js
+++ data/items.js
@@ -3,16 +3,19 @@ const items = [
         name: 'Item1',
         imageUrl: '/img/gift.jpeg',
         imageAlt: 'a wrapped gift',
+        buylink: 'https://google.com/',
         description: 'A nicely boxed and wrapped gift.'
     }, {
         name: 'Item1',
         imageUrl: '/img/gift.jpeg',
         imageAlt: 'a wrapped gift',
+        buylink: 'https://google.com/',
         description: 'A nicely boxed and wrapped gift.'
     }, {
         name: 'Item1',
         imageUrl: '/img/gift.jpeg',
         imageAlt: 'a wrapped gift',
+        buylink: 'https://google.com/',
         description: 'A nicely boxed and wrapped gift.'
     }
 ]
