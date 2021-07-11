6baad769affec2234e0ab640134213a614905839
EdwinAgbakpe <kagbakpe@gmail.com>
2021-07-11 18:40:58 +0000

    add extended button

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,9 +1,21 @@
 import React , {useState} from 'react';
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import { makeStyles } from '@material-ui/core/styles';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import Fab from '@material-ui/core/Fab';
 
-export default function ReservedCircleButton (){
+const useStyles = makeStyles((theme) => ({
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+      },
+    },
+    extendedIcon: {
+      marginRight: theme.spacing(1),
+    },
+  }));
+
+export default function ReservedButtonCircle (){
 
     const [isReserved, setIsReserved] = useState(false)
 
@@ -17,3 +29,15 @@ export default function ReservedCircleButton (){
         </Fab>
     )
 }
+
+function ReserveButtonExtended(){
+    const classes = useStyles();
+    return (
+        <Fab variant="extended" style={{background: "#4E962C", color:"#fff", height:"36px"}}>
+            <FavoriteBorderIcon className={classes.extendedIcon} />
+            Reserve
+        </Fab>
+    )
+}
+
+export {ReserveButtonExtended}
--- components/Card.jsx
+++ components/Card.jsx
@@ -1,8 +1,8 @@
 import Image from 'next/image'
-import Button from './Button'
+import Button, {ReserveButtonExtended} from './Button'
 export default function Card () {
     return(
-        <div className="card" style={{width: '18rem', borderRadius:'0.5rem'}}>
+        <div className="card" style={{width: '22rem', borderRadius:'0.5rem'}}>
             <div className="card-body relative">
                 <div className="grid grid-cols-3 justify-between">
                     <Image src="/img/gift.jpeg" alt="img" height="75" width="75" className="rounded col-span-1"/>
@@ -14,8 +14,9 @@ export default function Card () {
                 <p className="card-text py-6">Some description of the item.</p>
                 <a href="#" className="card-link text-purple-600">View Item &gt;</a>
                 </div>
-                <div className="flex justify-end absolute bottom-2.5 right-2.5">
-                <Button />
+                <div className="flex justify-end absolute bottom-2.5 right-2.5 space-x-2.5">
+                {/* <Button /> */}
+                <ReserveButtonExtended />
                 </div>
                 <br/>
             </div>
